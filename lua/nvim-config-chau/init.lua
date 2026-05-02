a54e91cb0662586936b5adb9f42c78a9d787259f
Chau Duong <duongdominhchau@gmail.com>
2026-05-02 15:00:30 +0700

    Wrap `init` in a module with `setup` function

--- lua/nvim-config-chau/init.lua
+++ lua/nvim-config-chau/init.lua
@@ -1,12 +1,18 @@
--- Enable Lua module loader for better performance
-vim.loader.enable()
+local M = {}
 
-require('nvim-config-chau.pack')
-require('nvim-config-chau.completion')
-require('nvim-config-chau.lsp')
-require('nvim-config-chau.treesitter')
-require('nvim-config-chau.theme')
-require('nvim-config-chau.code_action')
-require('nvim-config-chau.quick_fix')
-require('nvim-config-chau.filetree')
-require('nvim-config-chau.editor')
+function M.setup()
+    -- Enable Lua module loader for better performance
+    vim.loader.enable()
+
+    require('nvim-config-chau.pack')
+    require('nvim-config-chau.completion')
+    require('nvim-config-chau.lsp')
+    require('nvim-config-chau.treesitter')
+    require('nvim-config-chau.theme')
+    require('nvim-config-chau.code_action')
+    require('nvim-config-chau.quick_fix')
+    require('nvim-config-chau.filetree')
+    require('nvim-config-chau.editor')
+end
+
+return M
